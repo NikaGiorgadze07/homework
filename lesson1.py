36cbc225b0aff171900025bc721694e5bc8644e0
NikaGiorgadze07 <112762247+NikaGiorgadze07@users.noreply.github.com>
2022-09-21 18:45:34 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+I = "მე"
+am = "ვარ"
+name = "ნიკა"
+my = "ჩემი"
+gvari = "გვარი"
+Is = "არის"
+surname = "გიორგაძე"
+I = "მე"
+am = "ვარ"
+age = 10
+years = "წლის"
+my = "ჩემი"
+simagle = "სიმაღლე"
+Is = "არის"
+height = 141
+sm = "სანტიმეტრი"
+
+print(type(I))
+print(type(am))
+print(type(name))
+print(type(my))
+print(type(gvari))
+print(type(Is))
+print(type(surname))
+print(type(I))
+print(type(am))
+print(type(age))
+print(type(years))
+print(type(my))
+print(type(simagle))
+print(type(Is))
+print(type(height))
+print(type(sm))
